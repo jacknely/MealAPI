c31f618881e875c7bdef65db15a94edb2e208c83
jacknely <jackn926@gmail.com>
2019-07-31 23:27:15 +0100

    added doc strings and type hints

--- app/app.py
+++ app/app.py
@@ -19,12 +19,25 @@ class Recipes:
         self.recipes = recipes
 
     def filter_recipes_id(self, id: int) -> dict:
+        """
+        filters a dataframe of recipes by a given id
+        :param id: int
+        :return: dataframe
+        """
         a_recipe = self.recipes.loc[id]
         a_recipe = a_recipe.to_dict()
         a_recipe = self.convert_int_type(a_recipe)
         return a_recipe
 
-    def filter_recipes_cuisine(self, cuisine: str, page: int, items: int):
+    def filter_recipes_cuisine(self, cuisine: str, page: int, items: int) -> tuple:
+        """
+        filters a DataFrame of recipes by cuisine and
+        returns results by user define pagination
+        :param cuisine: str
+        :param page: int
+        :param items: int
+        :return: dataframe
+        """
         recipes = self.recipes.loc[self.recipes['recipe_cuisine'] == cuisine,
                                    ['title', 'marketing_description']]
         total = len(recipes.index)
@@ -35,7 +48,7 @@ class Recipes:
         return cuisine_recipes, total
 
     @staticmethod
-    def filter_recipes_to_dict(recipes):
+    def filter_recipes_to_dict(recipes: pd.DataFrame) -> list:
         cuisine_recipes = []
         for index, row in recipes.iterrows():
             recipe = recipes.loc[index]
@@ -45,20 +58,20 @@ class Recipes:
         return cuisine_recipes
 
     @staticmethod
-    def get_recipes_by_index(recipes, first, last):
+    def get_recipes_by_index(recipes: pd.DataFrame, first: int, last: int) -> pd.DataFrame:
         recipes = recipes[first:last]
         return recipes
 
     @staticmethod
-    def convert_int_type(dict):
+    def convert_int_type(recipe_dict: dict) -> dict:
         int_headers = ['id', 'calories_kcal', 'protein_grams', 'fat_grams', 'carbs_grams',
                        'preparation_time_minutes', 'shelf_life_days', 'gousto_reference']
-        for k, v in dict.items():
+        for k, v in recipe_dict.items():
             if k in int_headers:
-                dict[k] = int(v)
-        return dict
+                recipe_dict[k] = int(v)
+        return recipe_dict
 
-    def update_recipe(self, id, args):
+    def update_recipe(self, id: int, args: dict) -> dict:
         a_recipe = self.recipes
         for key, value in args.items():
             a_recipe.loc[id, key] = value
@@ -71,14 +84,14 @@ class Metadata:
     def __init__(self):
         pass
 
-    def get_metadata(self, items, pages, total):
+    def get_metadata(self, items: int, pages: int, total: int):
         metadata = {'page': pages, 'per_page': items,
                     'total_count': total}
         links = self.get_link_data(items, pages, total)
         metadata = [links, metadata]
         return metadata
 
-    def get_link_data(self, items, pages, total):
+    def get_link_data(self, items: int, pages: int, total: int):
         previous = self.get_previous_page_link(pages)
         last = round(total / items)
         next_page = self.get_next_page_link(last, pages)
@@ -95,7 +108,7 @@ class Metadata:
         return links
 
     @staticmethod
-    def get_previous_page_link(pages):
+    def get_previous_page_link(pages: int):
         if pages is 1:
             previous = '1'
         else:
@@ -103,7 +116,7 @@ class Metadata:
         return previous
 
     @staticmethod
-    def get_next_page_link(last, pages):
+    def get_next_page_link(last: int, pages: int):
         if last == pages:
             next_page = str(last)
         else:
