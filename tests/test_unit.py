d13bb8283ebb907b500bb5ab93749fe21313ce6a
jacknely <jackn926@gmail.com>
2019-08-03 16:56:54 +0100

    created more tests

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -1,20 +1,22 @@
 import pytest
-import pandas as pd
+import sys
 from pathlib import Path
 from pandas.util.testing import assert_frame_equal
 
-from app.app import Recipes, Metadata
-import app.import_file as import_file
+sys.path.append('../MealAPI')
 
-test_recipe_load = pd.DataFrame({'a': [1, 2], 'b': [1, 2], 'c': ['test3', 'test4'], 'd': ['test1', 'test2']})
-test_recipe_load_index = test_recipe_load.set_index('a')
+from app.recipes import Recipes
+from app.metadata import Metadata
+from app.import_file import Files
+
+from tests.test_variables import *
 
 
-class TestMealAPI:
+class TestImportFile:
 
     def setup_method(self):
-        self.files = import_file.Files()
-        self.test_filename = Path.cwd() / 'test_recipe-data.csv'
+        self.files = Files()
+        self.test_filename = Path.cwd() / 'tests/test_recipe-data.csv'
 
     def test_import_from_csv(self):
         recipe_load = self.files.import_from_csv(self.test_filename)
@@ -26,3 +28,33 @@ class TestMealAPI:
         assert_frame_equal(recipe_load, test_recipe_load_index)
 
 
+class TestRecipes:
+
+    def setup_method(self):
+        self.files = Files()
+        self.test_filename = Path.cwd() / 'tests/test_recipe-data.csv'
+        recipe_load = self.files.import_from_csv(self.test_filename)
+        self.recipes = Recipes(recipe_load)
+
+    def test_filter_recipes_id(self):
+        a_recipe = self.recipes.filter_recipes_id(1)
+        assert a_recipe == a_recipe_id_test
+
+
+class TestMetadata:
+
+    def setup_method(self):
+        self.metadata = Metadata()
+
+    @pytest.mark.parametrize('items, pages, total, output', metatest)
+    def test_get_metadata(self, items, pages, total, output):
+        metadata = self.metadata.get_metadata(items, pages, total)
+        assert metadata == output
+
+    def test_get_previous_page_link(self):
+        previous_page = self.metadata.get_previous_page_link(3)
+        assert previous_page == '2'
+
+    def test_get_next_page_link(self):
+        previous_page = self.metadata.get_next_page_link(5, 3)
+        assert previous_page == '4'
